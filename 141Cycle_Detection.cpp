8b978a610d687aa66400dcdf9501f327999c7af5
Prithvi Nadagouda <128277042+Prithvi1254@users.noreply.github.com>
2024-03-06 20:03:38 +0530

    Create 141Cycle_Detection.cpp

---
+++ 141Cycle_Detection.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head==NULL) return false;
+        ListNode*slow=head;
+        ListNode*fast=head;
+        while(fast!=NULL and fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow) return true;
+        }
+        return false;
+    }
+};
